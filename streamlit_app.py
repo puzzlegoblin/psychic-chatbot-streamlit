12ef1f7a0e608f9bbd758d0b3d610b7f12d858ae
puzzlegoblin <67012844+puzzlegoblin@users.noreply.github.com>
2025-03-17 22:22:48 +0000

    sqlite of course, same eternal problem

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import os
 import streamlit as st
-import pysqlite3 as sqlite3
+import pysqlite3
+import sys
+sys.modules['sqlite3'] = pysqlite3
 
 st.write("LD_LIBRARY_PATH:", os.environ.get("LD_LIBRARY_PATH"))
 st.write("SQLite version:", sqlite3.sqlite_version)
